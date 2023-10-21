92f6e83f30b7fd3cdc8b70c5c277be7a8da5a753
Sebastian <roncancio.juan@correounivalle.edu.co>
2023-10-21 14:40:45 -0500

    creacion del modelo d ela base de datos

--- automatic_notifications/models.py
+++ automatic_notifications/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class mail_info(models.Model):
+    user_id = models.CharField(null=False,unique=True,max_length=40,verbose_name='user_id')
+    subject = models.CharField(null=False,max_length=40,verbose_name='subject')
+    message = models.CharField(null=False,max_length=40,verbose_name='message')
+
+    def __str__(self):
+        return self.user_id
+    
+    class Meta:
+        db_table = 'mail_info'
+        verbose_name = 'Mail_Info'
+        verbose_name_plural = "mails_info"
+        ordering = ['id']
--- automatic_notifications/views.py
+++ automatic_notifications/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse, JsonResponse
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from .models import mail_info
 
 # Create your views here.
 def send_email(request):
+
+
     from_email = "eledenhelper@gmail.com"
     password = "kcgu zmff bmxi xlxe"
 
@@ -31,6 +34,9 @@ def send_email(request):
     server.sendmail(from_email, to_email, mail_structure.as_string())
 
     server.quit()
+
+    new_mail_info = mail_info.objects.create(user_id="1",subject=subject,message=message)
+    new_mail_info.save()
 
     return JsonResponse({"status": "Correo enviado exitosamente"})
 
